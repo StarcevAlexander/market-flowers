c363a88f829e35a01296cb10bc96a21f7690a827
StarcevAlexander <116572797+StarcevAlexander@users.noreply.github.com>
2024-01-11 16:02:44 +0800

    some sorting and checkbox-magic

--- frontend/src/app/shared/components/category-filter/category-filter.component.ts
+++ frontend/src/app/shared/components/category-filter/category-filter.component.ts
@@ -1,21 +1,72 @@
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryWithTypeType } from './../../../../types/category-with-type.type';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActiveParamsType } from 'src/types/active-params.type';
+import { ActiveParamsUtil } from '../../utils/active-params.util';
 
 @Component({
   selector: 'category-filter',
   templateUrl: './category-filter.component.html',
   styleUrls: ['./category-filter.component.scss'],
 })
-export class CategoryFilterComponent {
-  constructor(private router: Router) {}
+export class CategoryFilterComponent implements OnInit {
 
   @Input() categoryWithTypes: CategoryWithTypeType | null = null;
   @Input() type: string | null = null;
 
   open = false;
   activeParams: ActiveParamsType = { types: [] };
+
+  from: number | null = null;
+  to: number | null = null;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.activeParams = ActiveParamsUtil.processParams(params);
+
+      if (this.type) {
+        if (this.type === 'height') {
+          this.open = !!(
+            this.activeParams.heightFrom || this.activeParams.heightTo
+          );
+
+          this.from = this.activeParams.heightFrom
+            ? +this.activeParams.heightFrom
+            : null;
+          this.to = this.activeParams.heightTo
+            ? +this.activeParams.heightTo
+            : null;
+        } else if (this.type === 'diameter') {
+          this.open = !!(
+            this.activeParams.diameterFrom || this.activeParams.diameterTo
+          );
+
+          this.from = this.activeParams.diameterFrom
+            ? +this.activeParams.diameterFrom
+            : null;
+          this.to = this.activeParams.diameterTo
+            ? +this.activeParams.diameterTo
+            : null;
+        }
+      } else {
+        if (params['types'] && Array.isArray(params['types'])) {
+          this.activeParams.types = params['types'];
+          if (
+            this.categoryWithTypes &&
+            this.categoryWithTypes.types &&
+            this.categoryWithTypes.types.length > 0 &&
+            this.categoryWithTypes.types.some((type) => {
+              return this.activeParams.types.find((item) => type.url === item);
+            })
+          ) {
+            this.open = true;
+          }
+        }
+      }
+    });
+  }
 
   get title(): string {
     if (this.categoryWithTypes) {
@@ -44,12 +95,31 @@ export class CategoryFilterComponent {
           (item) => item !== url
         );
       } else if (!existingTypeInParams && checked) {
-        this.activeParams.types.push(url);
+        // баг ангуляра
+        // this.activeParams.types.push(url);
+        this.activeParams.types = [...this.activeParams.types, url];
       }
     } else if (checked) {
       this.activeParams.types = [url];
     }
 
     this.router.navigate(['/catalog'], { queryParams: this.activeParams });
+  }
+
+  updateFilterParamFromTo(param: string, value: string) {
+    if (
+      param === 'heightFrom' ||
+      param === 'heightTo' ||
+      param === 'diameterFrom' ||
+      param === 'diameterTo'
+    ) {
+      if (this.activeParams[param] && !value) {
+        delete this.activeParams[param];
+      } else if ((this.activeParams[param] = value)) {
+        this.activeParams[param] = value;
+      }
+
+      this.router.navigate(['/catalog'], { queryParams: this.activeParams });
+    }
   }
 }
--- frontend/src/app/views/product/catalog/catalog.component.ts
+++ frontend/src/app/views/product/catalog/catalog.component.ts
@@ -1,8 +1,12 @@
+import { AppliedFilterType } from './../../../../types/applied-filter.type';
 import { ProductType } from 'src/types/product.type';
 import { ProductService } from '../../../shared/services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/shared/services/category.service';
 import { CategoryWithTypeType } from '../../../../types/category-with-type.type';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ActiveParamsUtil } from 'src/app/shared/utils/active-params.util';
+import { ActiveParamsType } from 'src/types/active-params.type';
 
 @Component({
   selector: 'app-catalog',
@@ -12,18 +16,100 @@ import { CategoryWithTypeType } from '../../../../types/category-with-type.type'
 export class CatalogComponent implements OnInit {
   products: ProductType[] = [];
   categoriesWithTypes: CategoryWithTypeType[] = [];
+  activeParams: ActiveParamsType = { types: [] };
+  appliedFilters: AppliedFilterType[] = [];
+  sortingOpen = false;
+
+  sortingOptions: { name: string; value: string }[] = [
+    { name: 'От А до Я', value: 'az-asc' },
+    { name: 'От Я до А', value: 'az-desc' },
+    { name: 'По возрастанию цены', value: 'price-asc' },
+    { name: 'По убыванию цены', value: 'price-desc' },
+  ];
+
   constructor(
     private productService: ProductService,
-    private categotyService: CategoryService
+    private categotyService: CategoryService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
+    this.categotyService.getCategoriesWithTypes().subscribe((data) => {
+      this.categoriesWithTypes = data;
+
+      this.activatedRoute.queryParams.subscribe((params) => {
+        this.activeParams = ActiveParamsUtil.processParams(params);
+        this.appliedFilters = [];
+
+        this.activeParams.types.forEach((url) => {
+          for (let i = 0; i < this.categoriesWithTypes.length; i++) {
+            const foundType = this.categoriesWithTypes[i].types.find(
+              (type) => type.url === url
+            );
+            if (foundType) {
+              this.appliedFilters.push({
+                name: foundType.name,
+                urlParam: foundType.url,
+              });
+            }
+          }
+        });
+
+        if (this.activeParams.heightFrom) {
+          this.appliedFilters.push({
+            name: 'Высота: от ' + this.activeParams.heightFrom + ' см',
+            urlParam: 'heightFrom',
+          });
+        }
+        if (this.activeParams.heightTo) {
+          this.appliedFilters.push({
+            name: 'Высота: до ' + this.activeParams.heightTo + ' см',
+            urlParam: 'heightTo',
+          });
+        }
+        if (this.activeParams.diameterFrom) {
+          this.appliedFilters.push({
+            name: 'Диаметр: от ' + this.activeParams.diameterFrom + ' см',
+            urlParam: 'diameterFrom',
+          });
+        }
+        if (this.activeParams.diameterTo) {
+          this.appliedFilters.push({
+            name: 'Диаметр: до ' + this.activeParams.diameterTo + ' см',
+            urlParam: 'diameterTo',
+          });
+        }
+      });
+    });
+
     this.productService.getProducts().subscribe((data) => {
       this.products = data.items;
     });
+  }
 
-    this.categotyService.getCategoriesWithTypes().subscribe((data) => {
-      this.categoriesWithTypes = data;
-    });
+  removeAppliedFilter(appliedFilter: AppliedFilterType) {
+    if (
+      appliedFilter.urlParam === 'heightFrom' ||
+      appliedFilter.urlParam === 'heightTo' ||
+      appliedFilter.urlParam === 'diameterFrom' ||
+      appliedFilter.urlParam === 'diameterTo'
+    ) {
+      delete this.activeParams[appliedFilter.urlParam];
+    } else {
+      this.activeParams.types = this.activeParams.types.filter(
+        (item) => item !== appliedFilter.urlParam
+      );
+    }
+    this.router.navigate(['/catalog'], { queryParams: this.activeParams });
+  }
+
+  toggleSorting() {
+    this.sortingOpen = !this.sortingOpen;
+  }
+
+  sort(value: string) {
+    this.activeParams.sort = value;
+    this.router.navigate(['/catalog'], { queryParams: this.activeParams });
   }
 }
