c649c4197da6d0bccd7dbe9c5653ec894e29a981
StarcevAlexander <116572797+StarcevAlexander@users.noreply.github.com>
2024-01-14 08:29:55 +0800

    removing from cart

--- frontend/src/app/shared/components/product-card/product-card.component.ts
+++ frontend/src/app/shared/components/product-card/product-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProductType } from 'src/types/product.type';
 import { environment } from '../../../../environments/environment.production';
+import { CartService } from '../../services/cart.service';
+import { CartType } from 'src/types/cart.type';
 
 @Component({
   selector: 'product-card',
@@ -10,8 +12,33 @@ import { environment } from '../../../../environments/environment.production';
 export class ProductCardComponent implements OnInit {
   @Input() product!: ProductType;
   @Input() isLight: boolean = false;
+  isInCart: boolean = false;
+
   serverStaticPath = environment.serverStaticPath;
   count: number = 1;
-  constructor() {}
+
+  constructor(private cartService: CartService) {}
+
   ngOnInit(): void {}
+
+  updateCount(value: number) {
+    // this.count = value
+  }
+
+  addToCart() {
+    this.cartService
+      .updateCart(this.product.id, this.count)
+      .subscribe((data: CartType) => {
+        this.isInCart = true;
+      });
+  }
+
+  removeFromCart() {
+    this.cartService
+      .updateCart(this.product.id, 0)
+      .subscribe((data: CartType) => {
+        this.isInCart = false;
+        this.count = 1;
+      });
+  }
 }
